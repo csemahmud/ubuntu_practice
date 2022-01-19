2b347feee9d12d6654275b1e8e219d2824637fe7
khancse <khan@linkstaff.co.jp>
2022-01-19 17:26:42 +0900

    Eclipse Umfinished Commit

--- java/se/ApartmentLayeredJDBCApp/src/dal/ApartmentGateway.java
+++ java/se/ApartmentLayeredJDBCApp/src/dal/ApartmentGateway.java
@@ -6,6 +6,11 @@ package dal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.khan.ApartmentLayeredJDBCApp.models.ApartmentDAO;
 
@@ -14,7 +19,7 @@ import com.khan.ApartmentLayeredJDBCApp.models.ApartmentDAO;
  *
  */
 public class ApartmentGateway extends RootGateway {
-	
+
 	private final String TBL_APARTMENT = "tbl_apartment";
 	private final String ID = "id";
 	private final String NAME = "name";
@@ -45,7 +50,7 @@ public class ApartmentGateway extends RootGateway {
 	
 	public int createTable() {
 		
-		try(Connection conn = DriverManager.getConnection(DBURL, PASSWORD, DBURL)){
+		try(Connection conn = DriverManager.getConnection(DBURL, USERNAME, PASSWORD)){
 			
 			PreparedStatement headStatement = conn.prepareStatement(HEADING_RAW);
 			headStatement.executeUpdate();
@@ -68,14 +73,81 @@ public class ApartmentGateway extends RootGateway {
 		return -10;
 	}
 	
-	public int insertApartment(ApartmentDAO apartment) {
+	public int insertApartment(ApartmentDAO apartment) throws SQLException {
 		
 		String sql = "INSERT INTO "
 				+ TBL_APARTMENT + " ("
 				+ NAME + ", "
 				+ PRICE + ") VALUES (?, ?);";
-		try(Connection conn = )
+		try(Connection conn = DriverManager.getConnection(DBURL, USERNAME, PASSWORD)){
+			
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, apartment.getName());
+			statement.setString(2, Integer.toString(apartment.getPrice()));
+			return statement.executeUpdate();
+			
+		} catch (SQLException ex) {
+			// TODO Auto-generated catch block
+			
+			ex.printStackTrace();
+			throw ex;
+			
+		} catch (Exception ex) {
+			// TODO: handle exception
+			
+			ex.printStackTrace();
+			
+		}
 		return -10;
 	}
+	
+	public List<ApartmentDAO> selectAllApartments(){
+		
+		List<ApartmentDAO> allApartments = new ArrayList<>();
+		
+		String sql = "SELECT * FROM " + TBL_APARTMENT;
+		
+		try(Connection conn = DriverManager.getConnection(DBURL, USERNAME, PASSWORD)){
+			
+			Statement statement = conn.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				allApartments.add(new ApartmentDAO(
+						result.getInt(1), result.getString(NAME),
+						result.getInt(3), result.getDate(DATE)));
+			}
+			
+		} catch (SQLException ex) {
+			
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+			
+		} catch (Exception ex) {
+			
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+			
+		}
+		
+		return allApartments;
+		
+	}
+	
+	/**
+		 * @author KHAN MAHMUDUL HASAN CSE BD JP
+		 *
+		 */
+	public class SelectedStudent {
+
+		/**
+		 * 
+		 */
+		public SelectedStudent() {
+			// TODO Auto-generated constructor stub
+		}
+
+	}
+	
+	
 
 }
