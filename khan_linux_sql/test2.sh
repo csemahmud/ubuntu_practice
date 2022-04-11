a4f7a709bd04f97c522ef2af3dd2a5c6bb4d9b53
csemahmud <cse.mahmudul@gmail.com>
2022-04-11 20:08:47 +0900

    Practiced Shell Script Function from new Home Ubuntu

--- khan_linux_sql/test2.sh
+++ khan_linux_sql/test2.sh
@@ -33,9 +33,99 @@ sudo echo 'It\'s Shell Programming
 sudo echo 'It\'s Shell Programming''
 
 VAR=KHAN
-echo '$VAR owes <-$1500.**>; [ as of (`date +%m/%d`) ]'
+sudo echo '$VAR owes <-$1500.**>; [ as of (`date +%m/%d`) ]'
 VAR=KHAN
 echo "$VAR owes <-\$1500.**>; [ as of (`date +%m/%d`) ]"
 
 DATE=`date`
-echo "Current Date: $DATE"
+sudo echo "Current Date: $DATE"
+
+who > users
+sudo cat users
+echo line 1 > users
+sudo cat users
+sudo wc -l users
+users
+command << delimiter
+document
+delimiter
+
+
+sudo wc -l << EOF
+   This is a simple lookup program 
+	for good (and bad) restaurants
+	in Cape Town.
+EOF
+
+sudo cat << EOF
+This is a simple lookup program 
+for good (and bad) restaurants
+in Cape Town.
+EOF
+
+
+filename=test.txt
+sudo vi $filename <<EndOfCommands
+i
+This file was created automatically from
+a shell script
+^[
+ZZ
+EndOfCommands
+
+
+sudo cat test.txt
+sudo rm users
+sudo rm '*test.txt*'
+
+command > /dev/null
+command > /dev/null 2>&1
+sudo echo message 1>&2
+
+
+# Define your function here
+Hello () {
+   sudo echo "Hello World"
+}
+
+# Invoke your function
+Hello
+
+# Define your function here
+Hello () {
+   sudo echo "Hello World $1 $2"
+}
+
+# Invoke your function
+Hello Khan Mahmud
+
+# Define your function here
+Hello () {
+   sudo echo "Hello World $1 $2"
+   return 10
+}
+
+# Invoke your function
+Hello Khan Mahmud
+
+# Capture value returnd by last command
+ret=$?
+
+sudo echo "Return value is $ret"
+
+
+# Calling one function from another
+number_one () {
+   sudo echo "This is the first function speaking..."
+   number_two
+}
+
+number_two () {
+   sudo echo "This is now the second function speaking..."
+}
+
+# Calling function one.
+number_one
+
+unset -f number_one
+number_one
