0bc428f13cd2490c9b5ba2b405d8c6e5fd2a771f
KHAN MAHMUDUL HASAN <cse.mahmudul@gmail.com>
2023-01-30 17:13:32 +0900

    2023-01-30 Added react_sample3 from office windows

---
+++ java/spring_boot/react_sample3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserTable from './UserTable';
+
+function App() {
+  return (
+    <div className="App">
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ java/spring_boot/react_sample3/src/UserTable.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+const UserTable = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch(
+        'http://localhost:9191/users'
+        //'http://localhost:3100/users'
+      );
+      const users = await response.json();
+      setUsers(users);
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div style={{ margin: '2em' }}>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Domain</th>
+            <th>Age</th>
+            <th>Experience</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user.lastName}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.domain}</td>
+              <td>{user.age}</td>
+              <td>{user.experience}</td>
+              <td>{user.salarynpm}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserTable;
