8cb956aa77bbe70d2316e79887c6220673305282
csemahmud <lone.wolf.mahmud@gmail.com>
2025-07-05 13:09:29 +0900

    2025-07-05� Set up personal SSH identity and updated submodule state

--- set_personal_git_identity.sh
+++ set_personal_git_identity.sh
@@ -5,7 +5,7 @@ PERSONAL_NAME="csemahmud"
 PERSONAL_EMAIL="lone.wolf.mahmud@gmail.com"
 PERSONAL_GITHUB_HOST="github.com-personal"
 PERSONAL_SSH_KEY="$HOME/.ssh/id_ed25519_github_personal"
-REPO_PATH="$HOME/Documents/Work/ubuntu_practice"
+REPO_PATH="/d/Work/ubuntu_practice"
 GITHUB_REPO_PATH="csemahmud/ubuntu_practice.git"
 
 echo "🛠️ Starting personal Git configuration..."
