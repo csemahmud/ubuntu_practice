09953c21dd19b2b7b5cb73ad6d73676474bea69d
unknown <lone.wolf.mahmud@gmail.com>
2025-08-23 09:31:48 +0900

    2025-08-23 added devtools feature to LogInMySQL2508App

--- java/spring_boot/LogInMySQL2508App/src/test/java/com/khan/LogInMySQL2508App/LogInMySQL2508AppApplicationTest.java
+++ java/spring_boot/LogInMySQL2508App/src/test/java/com/khan/LogInMySQL2508App/LogInMySQL2508AppApplicationTest.java
@@ -3,10 +3,13 @@ package com.khan.LogInMySQL2508App;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Unit test for simple LogInMySQL2508AppApplication.
  */
+
+@SpringBootTest
 public class LogInMySQL2508AppApplicationTest {
 
     /**
